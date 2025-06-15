aa0fc903594359e7e65bc4402d524a35fdc851f6
NikitA <mohseninikita9@gmail.com>
2025-06-15 13:37:56 +0300

    create branch

--- HomeFuBack/obj/Release/net8.0/HomeFuBack.AssemblyInfo.cs
+++ HomeFuBack/obj/Release/net8.0/HomeFuBack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeFuBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e2127454da778f8a2600ae855fd95939ed3763")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f7a3bd714a96be0682e7037bba604945560db51")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeFuBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeFuBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
